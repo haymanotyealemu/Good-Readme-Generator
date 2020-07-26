9d9302c1573e646100fb07019fc16d08901bb96c
Haymanot W <haymanotalemu21@gmail.com>
2020-07-26 14:46:11 -0400

    calling Github API using axios

--- README.md
+++ README.md
@@ -1,25 +1,33 @@
-# Good Readme Generator
-![License](https://img.shields.io/badge/License--blue.svg)
-## Description
-command line application which generates a good readme for the project
-## Table of Contents (Optional)
-* [Installation](#installation)
-* [Usage](#usage)
-* [Credits](#credits)
-* [License](#license)
-## Installation
-clone the repo and run it 
-## Usage
-generates the readme for your project
-## Credits
-## License
-[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
-## Badges
-![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
-## Contributing 
-please email to me 
-## Tests
-none
-## Questions
-![image of me](https://avatars2.githubusercontent.com/u/43423292?s=460&u=26cb426de077fa987aa3198e029a68dfe04424c1&v=4)
-[haymanotalemu21@gmail.com](#email)
+
+    # hgkshlnckj
+    ##undefined
+    ## Description
+    something
+
+    ## Table of Contents (Optional)
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [Credits](#credits)
+    * [License](#license)
+
+    ## Installation
+    clone and run
+
+    ## Usage
+    generating a readme
+    ## Credits
+    ## License
+    [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+    ## Badges
+    ![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
+    ## Contributing
+    none
+    ## Test
+    none
+    ## Questions
+    ![Image of me](https://avatars0.githubusercontent.com/u/43423292?v=4)
+    [GitHub Profile](https://github.com/haymanotyealemu)
+    
+    [email](undefined)
+
+    
--- index.js
+++ index.js
@@ -1,100 +1,74 @@
 const inquirer = require("inquirer");
+const axios = require("axios");
 const fs = require("fs");
 const util = require("util");
+const generate = require("./utils/generateMarkdown");
+
 const writeFileAsync = util.promisify(fs.writeFile);
-function promptUser() {
-  return inquirer.prompt([
+const questions = [
+  {
+    type: "input",
+    message: "What is the title of your project?",
+    name: "title"
+    },
+    {
+    type: "input",
+    message: "Enter a description of your project",
+    name: "description"
+    },
+    {
+    type: "input",
+    message: "Enter any installation instructions",
+    name: "installation"
+    },
+    {
+    type: "input",
+    message: "Enter usage information",
+    name: "usage"
+    },
+    {
+    type: "checkbox",
+    name: "license",
+    choices: ["MIT License",
+              "GPL License","Public Domain (Unlicense)"
+            ]
+    },
+    {
+    type: "input",
+    message: "Enter contribution guidelines",
+    name: "contribution"  
+    },
+    {
+    type: "input",
+    message: "Enter test instructions",
+    name: "test"  
+    },
     {
       type: "input",
-      message: "What is the title of your project?",
-      name: "title"
-      },
-      {
+      message: "Enter your GitHub username",
+      name: "username"  
+    },
+
+    {
       type: "input",
-      message: "Enter a description of your project",
-      name: "description"
-      },
-      {
-      type: "input",
-      message: "Enter any installation instructions",
-      name: "installation"
-      },
-      {
-      type: "input",
-      message: "Enter usage information",
-      name: "usage"
-      },
-      {
-      type: "checkbox",
-      name: "license",
-      choices: ["MIT License",
-                "GPL License","Public Domain (Unlicense)"
-              ]
-      },
-      {
-      type: "input",
-      message: "Enter contribution guidelines",
-      name: "contribution"  
-      },
-      {
-      type: "input",
-      message: "Enter test instructions",
-      name: "test"  
-      },
-      {
-        type: "input",
-        message: "Enter your GitHub username",
-        name: "username"  
-      },
-      {
-        type: "input",
-        message: "Enter the url of your profile picture",
-        name: "profile"  
-      },
-      {
-        type: "input",
-        message: "Enter your Email address",
-        name: "email"  
-      }
-  ]);
-}
-function generateREADME(answers) {
-return`# ${answers.title}
-![License](https://img.shields.io/badge/License-${answers.license}-blue.svg)
-## Description
-${answers.description}
-## Table of Contents (Optional)
-* [Installation](#installation)
-* [Usage](#usage)
-* [Credits](#credits)
-* [License](#license)
-## Installation
-${answers.installation}
-## Usage
-${answers.usage}
-## Credits
-## License
-[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
-## Badges
-![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
-## Contributing 
-${answers.contribution}
-## Tests
-${answers.test}
-## Questions
-![image of me](https://avatars2.githubusercontent.com/u/43423292?s=460&u=26cb426de077fa987aa3198e029a68dfe04424c1&v=4)
-[${answers.email}](#email)
-`
-;
-}
-promptUser()
-  .then(function(answers) {
-    const readme = generateREADME(answers);
-    return writeFileAsync("README.md", readme);
-  })
-  .then(function() {
-    console.log("Successfully wrote to README.md");
-  })
-  .catch(function(err) {
-    console.log(err);
+      message: "Enter your Email address",
+      name: "email"  
+    }
+];
+inquirer.prompt(questions).then(function(data){
+  const queryUrl = `https://api.github.com/users/${data.username}`;
+  axios.get(queryUrl).then(function(res){
+    const githubInfo = {
+      githubImage: res.data.avatar_url,
+      profile: res.data.html_url,
+      name: res.data.name
+  };
+  return writeFileAsync("README.md", generate(data,githubInfo));
   });
+}).then(function() {
+  console.log("Successfully wrote to README.md");
+})
+.catch(function(err) {
+  console.log(err);
+});
+  
